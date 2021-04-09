7c80639415969964e8cc17bce8aa493ea68433b7
David Gurney <gurneyds@familysearch.org>
2021-04-09 14:21:53 -0600

    Prep for deployment

--- express/server.js
+++ express/server.js
@@ -14,6 +14,10 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
